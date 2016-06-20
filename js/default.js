2eb18f2faa06100947a4b8f59eeb38049cbe1307
jintan1123 <g1527040@st.t-kougei.ac.jp>
2016-06-20 17:11:57 +0900

    Create default.js

---
+++ js/default.js
@@ -0,0 +1,22 @@
+var number = Math.floor(Math.random()*3);
+var answer = parseInt(window.prompt('数当てゲーム。0~2の数字を入力してください！'));
+
+ver message;
+if(answer === numbe)
+ {
+  message = 'あたり！';
+ }
+else if(answer < number)
+ {
+  message = 'はずれ。もっと大きいです';
+ }
+else if(answer > number)
+ {
+  message = 'はずれ。もっと小さいです';
+ }
+ else
+ {
+  message = '0~3の数字を入力してください';
+ }
+ 
+ document.getElementByID('choice!).textContent = message;
